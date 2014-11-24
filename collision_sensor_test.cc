0c4d0c429571e29a76cd9447a96646d7b1b08f13
Said Achmiz <achmizs@gmail.com>
2014-11-24 01:35:20 -0500

    Unit test for collision sensor.

---
+++ collision_sensor_test.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include "NNCollisionSensor.h"
+#include "Matrix.h"
+#include "MatrixFunctions.h"
+using namespace std;
+
+int main()
+{
+	NNWorld* world;
+	NNRobot* robot;
+	
+	Matrix orientation = make3DRotationMatrix(PITCH, M_PI/2);
+	double offset = 0.1;	// 10 cm.
+	
+	NNAlgaeSensor sensor(orientation, offset);
+	sensor.setWorld(world);
+	sensor.setRobot(robot);
+	
+	sensor.printSensorValue();
+	cout << "Updating sensor value...\n";
+	sensor.updateSensorValue();
+	sensor.printSensorValue();
+	
+	return 0;
+}
