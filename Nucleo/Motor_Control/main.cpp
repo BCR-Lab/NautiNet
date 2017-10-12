444e11c640993359ba12064b607b76c85c3dc885
Davis Chen <31108424+davis-chen@users.noreply.github.com>
2017-10-12 14:36:31 -0400

    fixed Makefile and cleaned up main.cpp

--- Nucleo/Motor_Control/main.cpp
+++ Nucleo/Motor_Control/main.cpp
@@ -1,25 +1,18 @@
 #include "mbed.h"
 
 DigitalOut myled(LED1);
-//DigitalOut motor_IN1(D3);
 DigitalOut motor_IN2(D10);
 
-PwmOut in1(D3);
-//PwmOut in2(D10);
-
+PwmOut motor_IN1(D3);
 
 int main() {
 	float waittime = 2.0;
-	myled = 1;
 	motor_IN2 = 0;
-	//in1.period(0.020f);
+	//motor_IN1.period(0.020f);
 	float outputCycle = 0.0;
 
     while(1) {
-		myled = !myled;
-		//motor_IN1 = myled;
-		//motor_IN2 = myled;
-    	in1.write(outputCycle);
+    	motor_IN1.write(outputCycle);
 		if (outputCycle <= 0.9)
 			outputCycle += 0.1;
 		else
