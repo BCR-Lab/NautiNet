6a76b5d7fa19a6f384a866265ab1ae24ef3298ab
Davis Chen <31108424+davis-chen@users.noreply.github.com>
2017-11-05 18:04:02 -0500

    now prompts user to specify ramp up time, time at max, and ramp down time motor will run for 1 cycle and prompt user again

--- Nucleo/Motor_Control/main.cpp
+++ Nucleo/Motor_Control/main.cpp
@@ -3,22 +3,21 @@
  */
 
 
-
-
 #include "mbed.h"
 
 // length of 1 PWM cycle (in seconds)
 const float PWM_PERIOD = 0.001;
-const float ramp_step = 0.01;
+const float RAMP_STEP = 0.01;
 
-// these should be set up by user input
-const float RAMP_UP_TIME = 2.0;
-const float RAMP_DOWN_TIME = 5.0;
-const float TIME_AT_MAX = 2.5;
+float ramp_up_time;
+float ramp_down_time;
+float time_at_max;
 
 // Initialize output pins to motor driver
 PwmOut motor_IN1(D3);
 PwmOut motor_IN2(D4);
+
+Serial pc(SERIAL_TX, SERIAL_RX);
 
 /*
  * Ramps up motor from 0% to 100%
@@ -26,8 +25,8 @@ PwmOut motor_IN2(D4);
  *  ramp_duration - the amount of time in seconds that it will take to ramp from 0-100%
 */
 void rampUpMotor(float ramp_duration) {
-	float wait_interval = ramp_duration*ramp_step;
-	for (float i=0; i<1.0; i+=ramp_step) {
+	float wait_interval = ramp_duration*RAMP_STEP;
+	for (float i=0; i<1.0; i+=RAMP_STEP) {
 		motor_IN1.write(i);
 		wait(wait_interval);
 	}
@@ -39,8 +38,8 @@ void rampUpMotor(float ramp_duration) {
  *  ramp_duration - the amount of time in seconds that it will take to ramp from 100-0%
 */
 void rampDownMotor(float ramp_duration) {
-	float wait_interval = ramp_duration*ramp_step;
-	for (float i=1.0; i>0.0; i-=ramp_step) {
+	float wait_interval = ramp_duration*RAMP_STEP;
+	for (float i=1.0; i>0.0; i-=RAMP_STEP) {
 		motor_IN1.write(i);
 		wait(wait_interval);
 	}
@@ -52,9 +51,45 @@ int main() {
 	motor_IN2.write(0);
 
 	while(1) {
-		rampUpMotor(RAMP_UP_TIME);
-		wait(TIME_AT_MAX);
-		rampDownMotor(RAMP_DOWN_TIME);
-		wait(2); // this line will not be needed when user input is set up
+		char c[8];
+
+		int items_read = 0;
+		pc.printf("Enter ramp up time: ");
+		pc.gets(c, 8);
+		items_read = sscanf(c, "%f", &ramp_up_time);
+		while (items_read == 0) {
+			pc.printf("\nError: Not a number\n");
+			pc.printf("Enter ramp up time: ");
+			pc.gets(c, 8);
+			items_read = sscanf(c, "%f", &ramp_up_time);
+		}
+
+		pc.printf("\nEnter time at max: ");
+		pc.gets(c, 8);
+		items_read = sscanf(c, "%f", &time_at_max);
+		while (items_read == 0) {
+			pc.printf("\nError: Not a number\n");
+			pc.printf("Enter time at max: ");
+			pc.gets(c, 8);
+			items_read = sscanf(c, "%f", &time_at_max);
+		}
+
+		pc.printf("\nEnter ramp down time: ");
+		pc.gets(c, 8);
+		items_read = sscanf(c, "%f", &ramp_down_time);
+		while (items_read == 0) {
+			pc.printf("\nError: Not a number\n");
+			pc.printf("Enter ramp down time: ");
+			pc.gets(c, 8);
+			items_read = sscanf(c, "%f", &ramp_down_time);
+		}
+
+		pc.printf("\nRamping up for %f seconds\n", ramp_up_time);
+		rampUpMotor(ramp_up_time);
+		pc.printf("Max output for %f seconds\n", time_at_max);
+		wait(time_at_max);
+		pc.printf("Ramping down for %f seconds\n", ramp_down_time);
+		rampDownMotor(ramp_down_time);
+		//wait(2); // this line will not be needed when user input is set up
 	}
 }
