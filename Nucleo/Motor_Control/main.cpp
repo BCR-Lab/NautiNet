05e5fde2e1d724d79aef89d158fc692886799805
Davis Chen <31108424+davis-chen@users.noreply.github.com>
2017-11-07 14:53:13 -0500

    test

--- Nucleo/Motor_Control/main.cpp
+++ Nucleo/Motor_Control/main.cpp
@@ -9,10 +9,6 @@
 const float PWM_PERIOD = 0.001;
 const float RAMP_STEP = 0.01;
 
-float ramp_up_time;
-float ramp_down_time;
-float time_at_max;
-
 // Initialize output pins to motor driver
 PwmOut motor_IN1(D3);
 PwmOut motor_IN2(D4);
@@ -50,39 +46,46 @@ int main() {
 	// set to 0; motor_IN2 is used when running motor in reverse direction
 	motor_IN2.write(0);
 
+	float ramp_up_time;
+	float ramp_down_time;
+	float time_at_max;
+
+	int ramp_up_time_millis;
+	int ramp_down_time_millis;
+	int time_at_max_millis;
+
 	while(1) {
 		char c[8];
 
 		int items_read = 0;
 		pc.printf("Enter ramp up time: ");
 		pc.gets(c, 8);
-		items_read = sscanf(c, "%f", &ramp_up_time);
-		while (items_read == 0) {
+		while(sscanf(c, "%d", &ramp_up_time_millis) == 0) {
 			pc.printf("\nError: Not a number\n");
 			pc.printf("Enter ramp up time: ");
 			pc.gets(c, 8);
-			items_read = sscanf(c, "%f", &ramp_up_time);
 		}
 
 		pc.printf("\nEnter time at max: ");
 		pc.gets(c, 8);
-		items_read = sscanf(c, "%f", &time_at_max);
-		while (items_read == 0) {
+		while(sscanf(c, "%d", &time_at_max_millis) == 0) {
 			pc.printf("\nError: Not a number\n");
 			pc.printf("Enter time at max: ");
 			pc.gets(c, 8);
-			items_read = sscanf(c, "%f", &time_at_max);
 		}
 
 		pc.printf("\nEnter ramp down time: ");
 		pc.gets(c, 8);
-		items_read = sscanf(c, "%f", &ramp_down_time);
-		while (items_read == 0) {
+		while(sscanf(c, "%d", &ramp_down_time_millis) == 0) {
 			pc.printf("\nError: Not a number\n");
 			pc.printf("Enter ramp down time: ");
 			pc.gets(c, 8);
-			items_read = sscanf(c, "%f", &ramp_down_time);
 		}
+
+		ramp_up_time = (float) ramp_up_time_millis/1000.0;
+		time_at_max = (float) time_at_max_millis/1000.0;
+		ramp_down_time = (float) ramp_down_time_millis/1000.0;
+		pc.printf("\n%f",ramp_up_time/1000.0);
 
 		pc.printf("\nRamping up for %f seconds\n", ramp_up_time);
 		rampUpMotor(ramp_up_time);
